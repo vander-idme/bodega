33c59c446cc50d304317f436806857b13b62f5e8
magnerronny <rjimenezampuero@gmail.com>
2020-10-15 18:00:06 -0500

    subiendo frontend

--- src/models/Cliente.js
+++
@@ -1,9 +0,0 @@
-const mongoose= require('mongoose');
-let Schema=mongoose.Schema;
-let SchemaCliente = new Schema({
-    nombre: String,
-    telefono: Number,
-    direccion:String
-});
-
-module.exports=mongoose.model('Cliente',SchemaCliente);
